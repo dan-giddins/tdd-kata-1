9fb76464f09313b8dec7c077cb1b94163c4b5d61
Dan Giddins <daniel.giddins112@gmail.com>
2018-08-15 16:51:08 +0100

    Done task 6

--- tdd-kata-1-tests/StringCalculatorUnitTest.cs
+++ tdd-kata-1-tests/StringCalculatorUnitTest.cs
@@ -48,5 +48,75 @@ namespace tdd_kata_1_tests
         {
             Assert.Equal(15, stringCalculator.Add("1,2,3,4,5"));
         }
+
+        [Fact]
+        public void Add_TenElevenTwelve_ReturnThirtyThree()
+        {
+            Assert.Equal(33, stringCalculator.Add("10,11,12"));
+        }
+
+        [Fact]
+        public void Add_OneNewLineTwo_ReturnThree()
+        {
+            Assert.Equal(3, stringCalculator.Add("1\n2"));
+        }
+
+        [Fact]
+        public void Add_OneNewLineTwoCommaThree_ReturnSix()
+        {
+            Assert.Equal(6, stringCalculator.Add("1\n2,3"));
+        }
+
+        [Fact]
+        public void Add_OneNewLineTwoNewLineThreeCommaFourCommaFiveNewLineSixCommaSeven_ReturnFifteySix()
+        {
+            Assert.Equal(28, stringCalculator.Add("1\n2\n3,4,5\n6,7"));
+        }
+
+        [Fact]
+        public void Add_Delimiter_ReturnZero()
+        {
+            Assert.Equal(0, stringCalculator.Add("//;\n"));
+        }
+
+        [Fact]
+        public void Add_DelimiterOneTwo_ReturnThree()
+        {
+            Assert.Equal(3, stringCalculator.Add("//;\n1;2"));
+        }
+
+        [Fact]
+        public void Add_DelimiterOneNewLineTwoNewLineThreeCommaFourCommaFiveNewLineSixCommaSeven_ReturnTwentyEight()
+        {
+            Assert.Equal(28, stringCalculator.Add("//;\n1\n2\n3,4,5\n6,7"));
+        }
+        [Fact]
+        public void Add_DelimiterOneNewLineTwoNewLineThreeCommaNegativeFourCommaFiveNewLineSixCommaSeven_ReturnTwentyFour()
+        {
+            Assert.Equal(24, stringCalculator.Add("//;\n1\n2\n3,-4,5\n6,7"));
+        }
+
+        [Fact]
+        public void Add_DelimiterNegativeFour_ReturnZero()
+        {
+            Assert.Equal(0, stringCalculator.Add("//;\n-4"));
+        }
+
+        [Fact]
+        public void Add_NegativeFour_ReturnZero()
+        {
+            Assert.Equal(0, stringCalculator.Add("-4"));
+        }
+
+        [Fact]
+        public void Add_DelimiterOneThousandNewLineTwoNewLineThreeCommaNegativeFourCommaFiveNewLineSixCommaSeven_ReturnOneThousandAndTwentyThree()
+        {
+            Assert.Equal(1023, stringCalculator.Add("//;\n1000\n2\n3,-4,5\n6,7"));
+        }
+        [Fact]
+        public void Add_OneNewLineTwoNewLineThreeCommaNegativeFourCommaFiveNewLineSixCommaSevenDelimiter_ReturnTwentyTwo()
+        {
+            Assert.Equal(22, stringCalculator.Add("//;\n1\n2000\n3,-4,5\n6,7"));
+        }
     }
 }
--- tdd-kata-1/StringCalculator.cs
+++ tdd-kata-1/StringCalculator.cs
@@ -1,21 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace tdd_kata_1
 {
     public class StringCalculator
     {
+        private List<char> delimiters;
+        int result;
+
         public int Add(string numbers)
         {
+            delimiters = new List<char> { ',', '\n' };
+            if (numbers.Length > 3
+                && numbers.Substring(0, 2) == "//"
+                && numbers[3] == '\n')
+            {
+                delimiters.Add(numbers[2]);
+                numbers = numbers.Substring(4);
+            }
+
             if (numbers == "")
             {
                 return 0;
             }
-            string[] strings = numbers.Split(',');
-            int result = 0;
-            foreach (string s in strings)
+
+            List<string> stringList = new List<string>() { numbers };
+            foreach (char delimiter in delimiters)
             {
-                result += Int32.Parse(s);
+                List<string> tempStringList = new List<string>();
+                foreach (string s in stringList)
+                {
+                    tempStringList.AddRange(s.Split(delimiter));
+                }
+                stringList = tempStringList;
+            }
+
+            result = 0;
+            foreach (string s in stringList)
+            {
+                int i = Int32.Parse(s);
+                try
+                {
+                    if (i < 0)
+                    {
+                        throw new Exception("negatives not allowed: " + i);
+                    }
+                    if (!(i > 1000))
+                    {
+                        result += i;
+                    }
+                } catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             return result;
         }
